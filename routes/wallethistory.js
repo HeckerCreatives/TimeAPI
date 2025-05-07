9d0da5b49af01040175d008391793b3993b7b436
Joshupau <97903490+Joshupau@users.noreply.github.com>
2025-05-07 14:23:09 +0800

    poooosh

--- routes/wallethistory.js
+++ routes/wallethistory.js
@@ -1,8 +1,11 @@
 const router = require("express").Router()
-const { playerwallethistory, getwallettotalearnings, getwallettotalearningsforadmin, deleteplayerwallethistoryforadmin, getplayerwallethistoryforadmin, editplayerwallethistoryforadmin, createplayerwallethistoryforadmin } = require("../controllers/wallethistory")
+const { playerwallethistory, getwallettotalearnings, getwallettotalearningsforadmin, deleteplayerwallethistoryforadmin, getplayerwallethistoryforadmin, editplayerwallethistoryforadmin, createplayerwallethistoryforadmin, getwalletstatistics, getwalletstatisticssuperadmin } = require("../controllers/wallethistory")
 const { protectplayer, protectsuperadmin, protectadmin } = require("../middleware/middleware")
 
 router
+    .get("/getwalletstatistics", protectplayer, getwalletstatistics)
+    .get("/getwalletstatisticssuperadmin", protectsuperadmin, getwalletstatisticssuperadmin)
+
     .get("/userwallethistory", protectplayer, playerwallethistory)
     .get("/getwallettotalearnings", protectplayer, getwallettotalearnings)
     .get("/getwallettotalearningsforadmin", protectsuperadmin, getwallettotalearningsforadmin)
