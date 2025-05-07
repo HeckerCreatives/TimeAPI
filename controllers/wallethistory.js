e9958ef14bf86adf80445e155b76310fd7b2422c
Darel Honrejas <133852063+D4rzDev@users.noreply.github.com>
2025-05-07 14:21:59 +0800

    pooosh

--- controllers/wallethistory.js
+++ controllers/wallethistory.js
@@ -241,6 +241,175 @@ exports.getwallettotalearnings = async (req, res) => {
     return res.json({message: "success", data: finaldata})
 }
 
+exports.getwalletstatistics = async (req, res) => {
+    const {id, username} = req.user
+
+    const finaldata = {
+        time: 0,
+        referral: 0,
+        unilevel: 0
+    }
+
+    const statisticGame = await Wallethistory.aggregate([
+        { 
+            $match: { 
+                owner: new mongoose.Types.ObjectId(id), 
+                type: "chronocoinwallet" 
+            } 
+        },
+        { 
+            $group: { 
+                _id: null, 
+                totalAmount: { $sum: "$amount" } 
+            } 
+        }
+    ])
+    .catch(err => {
+        console.log(`There's a problem getting the statistics of earning game for ${username}. Error ${err}`)
+
+        return res.status(400).json({message: "bad-request", data : "There's a problem getting the statistics of earning game. Please contact customer support."})
+    })
+
+    if (statisticGame.length > 0) {
+        finaldata.game = statisticGame[0].totalAmount;
+    }
+
+    const statisticReferral = await Wallethistory.aggregate([
+        { 
+            $match: { 
+                owner: new mongoose.Types.ObjectId(id), 
+                type: "directcommissionwallet" 
+            } 
+        },
+        { 
+            $group: { 
+                _id: null, 
+                totalAmount: { $sum: "$amount" } 
+            } 
+        }
+    ])
+    .catch(err => {
+        console.log(`There's a problem getting the statistics of Referral for ${username}. Error ${err}`)
+
+        return res.status(400).json({message: "bad-request", data : "There's a problem getting the statistics of Referral. Please contact customer support."})
+    })
+
+    if (statisticReferral.length > 0) {
+        finaldata.referral = statisticReferral[0].totalAmount;
+    }
+
+    const statisticUnilevel = await Wallethistory.aggregate([
+        { 
+            $match: { 
+                owner: new mongoose.Types.ObjectId(id), 
+                type: "commissionwallet" 
+            } 
+        },
+        { 
+            $group: { 
+                _id: null, 
+                totalAmount: { $sum: "$amount" } 
+            } 
+        }
+    ])
+    .catch(err => {
+        console.log(`There's a problem getting the statistics of Unilevel ${username}. Error ${err}`)
+
+        return res.status(400).json({message: "bad-request", data : "There's a problem getting the statistics of Unilevel. Please contact customer support."})
+    })
+
+    if (statisticUnilevel.length > 0) {
+        finaldata.unilevel = statisticUnilevel[0].totalAmount;
+    }
+
+    return res.json({message: "success", data: finaldata})
+}
+
+
+exports.getwalletstatisticssuperadmin = async (req, res) => {
+    const {id} = req.query
+
+    const finaldata = {
+        time: 0,
+        referral: 0,
+        unilevel: 0
+    }
+
+    const statisticGame = await Wallethistory.aggregate([
+        { 
+            $match: { 
+                owner: new mongoose.Types.ObjectId(id), 
+                type: "chronocoinwallet" 
+            } 
+        },
+        { 
+            $group: { 
+                _id: null, 
+                totalAmount: { $sum: "$amount" } 
+            } 
+        }
+    ])
+    .catch(err => {
+        console.log(`There's a problem getting the statistics of earning game for ${username}. Error ${err}`)
+
+        return res.status(400).json({message: "bad-request", data : "There's a problem getting the statistics of earning game. Please contact customer support."})
+    })
+
+    if (statisticGame.length > 0) {
+        finaldata.game = statisticGame[0].totalAmount;
+    }
+
+    const statisticReferral = await Wallethistory.aggregate([
+        { 
+            $match: { 
+                owner: new mongoose.Types.ObjectId(id), 
+                type: "directcommissionwallet" 
+            } 
+        },
+        { 
+            $group: { 
+                _id: null, 
+                totalAmount: { $sum: "$amount" } 
+            } 
+        }
+    ])
+    .catch(err => {
+        console.log(`There's a problem getting the statistics of Referral for ${username}. Error ${err}`)
+
+        return res.status(400).json({message: "bad-request", data : "There's a problem getting the statistics of Referral. Please contact customer support."})
+    })
+
+    if (statisticReferral.length > 0) {
+        finaldata.referral = statisticReferral[0].totalAmount;
+    }
+
+    const statisticUnilevel = await Wallethistory.aggregate([
+        { 
+            $match: { 
+                owner: new mongoose.Types.ObjectId(id), 
+                type: "commissionwallet" 
+            } 
+        },
+        { 
+            $group: { 
+                _id: null, 
+                totalAmount: { $sum: "$amount" } 
+            } 
+        }
+    ])
+    .catch(err => {
+        console.log(`There's a problem getting the statistics of Unilevel ${username}. Error ${err}`)
+
+        return res.status(400).json({message: "bad-request", data : "There's a problem getting the statistics of Unilevel. Please contact customer support."})
+    })
+
+    if (statisticUnilevel.length > 0) {
+        finaldata.unilevel = statisticUnilevel[0].totalAmount;
+    }
+
+    return res.json({message: "success", data: finaldata})
+}
+
 //  #endregion
 
 // #region ADMIN
